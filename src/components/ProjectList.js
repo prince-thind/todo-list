edd8900fc45288cf93e0a3d57674589e4628a023
Prince-Thind <princethind799@gmail.com>
2021-07-17 20:26:05 +0530

    refactor activation logic

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,14 +2,14 @@ function ProjectList({ projects,setActiveProject }) {
   return (
     <ul className="project-list">
       {projects.map((project,index) => {
-        return <li onClick={activateProject} key={project.key} data-key={index}>{project.name}</li>
+          return <Project project={project} key={index}/>
       })}
     </ul>
   );
 
-  function activateProject(e){
-    setActiveProject(projects[e.target.getAttribute("data-key")])
-    
+  function Project({project}) {
+    return <li onClick={()=>{setActiveProject(project)}} key={project.key}>{project.name}</li>
+      
   }
 }
 export default ProjectList;

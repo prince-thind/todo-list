1965217bde72e8a42bbed4510f62836ea984c383
Prince-Thind <princethind799@gmail.com>
2021-07-17 20:25:28 +0530

    add keys to sampleProjects

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import { useState, useEffect } from "react";
 
+
+
 import ProjectList from "./components/ProjectList";
 import TaskList from "./components/TaskList";
 import StatusBar from "./components/StatusBar";
 
 function App() {
   const [projects, setProjects] = useState([]);
-  const [activeProject,setActiveProject]=useState(null);
+  const [activeProject, setActiveProject] = useState(null);
+  
 
   useEffect(() => {
     setProjects(getProjects());
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-
-  
 
   function getProjects() {
     return sampleProjects();
   }
+
   function sampleProjects() {
     return [
       {
         name: "project1",
-        key:1,
         tasks: [{ name: "task1.1" }, { name: "task1.2" }],
       },
       {
         name: "project2",
-        key:2,
         tasks: [{ name: "task2.1" }, { name: "task2.2" }],
       },
       {
         name: "project3",
-        key:3,
         tasks: [{ name: "task3.1" }, { name: "task3.2" }],
       },
     ];
@@ -46,8 +44,8 @@ function App() {
         <StatusBar />
       </header>
       <main>
-        <ProjectList projects={projects} setActiveProject={setActiveProject}/>
-        <TaskList activeProject={activeProject}  />
+        <ProjectList projects={projects} setActiveProject={setActiveProject} />
+        <TaskList activeProject={activeProject} />
       </main>
       <footer className="footer">CopyRight &copy; Prince Thind</footer>
     </div>

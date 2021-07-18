992c9c8456ab7aece9fe340de70001a64174ce78
Prince-Thind <princethind799@gmail.com>
2021-07-18 10:49:27 +0530

    update projectlist and tasklist

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,15 +1,32 @@
-function ProjectList({ projects,setActiveProject }) {
-  return (
-    <ul className="project-list">
-      {projects.map((project,index) => {
-          return <Project project={project} key={index}/>
-      })}
-    </ul>
-  );
+import ProjectForm from './ProjectForm';
 
-  function Project({project}) {
-    return <li onClick={()=>{setActiveProject(project)}} key={project.key}>{project.name}</li>
-      
+function ProjectList({ user,projects, setActiveProject }) {
+  if(user){
+      return (
+    <div className="project-tab">
+      <ul className="project-list">
+        {projects.map((project, index) => {
+          return <Project project={project} key={index} />;
+        })}
+      </ul>
+      <ProjectForm user={user}/>
+    </div>
+  );
+  }
+  else return null;
+
+
+  function Project({ project }) {
+    return (
+      <li
+        onClick={() => {
+          setActiveProject(project);
+        }}
+        key={project.key}
+      >
+        {project.name}
+      </li>
+    );
   }
 }
 export default ProjectList;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,45 @@
-function TaskList({activeProject}){
-    if(activeProject){
-     return  <List />
-    }
-    else{
-        return null;
-    }
-   
-    function List(){
-        return <ul className='task-list'>
-        {activeProject.tasks.map((task,index)=>{
-            return <li key={index}>{task.name}</li>
+import { useState } from "react";
+import TaskForm from "./TaskForm";
+
+function TaskList({ activeProject }) {
+  const [formActive, setFormActive] = useState(false);
+
+  if (activeProject) {
+    return (
+      <div className="task-bar">
+        <List />
+        <AddTaskButton formActive={formActive} />
+        <TaskForm formActive={formActive} setFormActive={setFormActive} activeProject={activeProject} />
+      </div>
+    );
+  } else {
+    return null;
+  }
+
+  function List() {
+    return (
+      <ul className="task-list">
+        {activeProject.tasks.map((task, index) => {
+          return <li key={index}>{task.name}</li>;
         })}
-    </ul>
+      </ul>
+    );
+  }
+
+  function AddTaskButton({ formActive }) {
+    if (!formActive) {
+      return (
+        <button
+          onClick={() => {
+            setFormActive(true);
+          }}
+        >
+          Add Task
+        </button>
+      );
     }
+    else return null;
+  }
 }
 
 export default TaskList;

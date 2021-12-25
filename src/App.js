ba41a957c2f4966a10c5456c2bba8bbde1f2da83
Prince Thind <Princethind799@gmail.com>
2021-12-25 16:32:48 +0530

    add fakerjs data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import ProjectList from "./components/ProjectList";
 import TaskList from "./components/TaskList";
 import Header from "./components/Header";
+import * as faker from 'faker';
+import uniqid from 'uniqid';
 
 function App() {
   const [projects, setProjects] = useState([]);
@@ -48,27 +50,22 @@ function App() {
 export default App;
 
 function sampleProjects() {
-  return [
-    {
-      name: "proejct1",
-      tasks: [
-        { name: "t11", description: "d11" },
-        { name: "t12", description: "d12" },
-      ],
-    },
-    {
-      name: "proejct2",
-      tasks: [
-        { name: "t21", description: "d21" },
-        { name: "t22", description: "d22" },
-      ],
-    },
-    {
-      name: "proejct3",
-      tasks: [
-        { name: "t31", description: "d31" },
-        { name: "t32", description: "d32" },
-      ],
-    },
-  ];
+  const projects=[];
+
+  for(let i=0;i<3;i++){
+    const project={};
+    project.name=`Sample Project ${i+1}`
+    project.id=uniqid();
+    const tasks=[];
+    for(let i=0;i<2;i++){
+      const task={};
+      task.name=faker.commerce.product();
+      task.description=faker.lorem.lines(10);
+      tasks.push(task);
+    }
+    project.tasks=tasks;
+    projects.push(project);
+  }
+
+  return projects;
 }

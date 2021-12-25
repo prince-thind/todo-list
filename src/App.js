a8383dd730644391cd7c80f25530ddb00fb06a4b
Prince Thind <Princethind799@gmail.com>
2021-12-25 14:17:51 +0530

    seperate Header into its own Component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import { useState, useEffect } from 'react';
-import ProjectList from './components/ProjectList';
-import TaskList from './components/TaskList';
-import StatusBar from './components/StatusBar';
+import { useState, useEffect } from "react";
+import ProjectList from "./components/ProjectList";
+import TaskList from "./components/TaskList";
+import Header from "./components/Header";
 
 function App() {
-  const [user, setUser] = useState(null);
   const [projects, setProjects] = useState([]);
   const [activeProject, setActiveProject] = useState(null);
 
   useEffect(() => {
-    setUser(getLocalUser());
     setProjects(getProjects());
   }, []);
 
   useEffect(() => {
     const jsonProjects = JSON.stringify(projects);
-    localStorage.setItem('todo-projects', jsonProjects);
+    localStorage.setItem("todo-projects", jsonProjects);
   }, [projects]);
 
-  function getLocalUser() {
-    if (localStorage.hasOwnProperty('todo-user')) {
-      return localStorage.getItem('todo-user');
-    }
-    return 'Guest';
-  }
-
   function getProjects(user) {
-    // return sampleProjects();
-    const jsonProjects=localStorage.getItem('todo-projects');
-    return JSON.parse(jsonProjects);    
+    return sampleProjects();
+    // const jsonProjects=localStorage.getItem('todo-projects');
+    // return JSON.parse(jsonProjects);
   }
 
   return (
     <div className="App">
-      <header className="header">
-        <h1 className="heading">Todo List</h1>
-        <StatusBar user={user} setUser={setUser} />
-      </header>
+      <Header />
+
       <main className="main">
         <ProjectList
           projects={projects}
@@ -58,28 +47,27 @@ function App() {
 
 export default App;
 
-
 function sampleProjects() {
   return [
     {
-      name: 'proejct1',
+      name: "proejct1",
       tasks: [
-        { name: 't11', description: 'd11' },
-        { name: 't12', description: 'd12' },
+        { name: "t11", description: "d11" },
+        { name: "t12", description: "d12" },
       ],
     },
     {
-      name: 'proejct2',
+      name: "proejct2",
       tasks: [
-        { name: 't21', description: 'd21' },
-        { name: 't22', description: 'd22' },
+        { name: "t21", description: "d21" },
+        { name: "t22", description: "d22" },
       ],
     },
     {
-      name: 'proejct3',
+      name: "proejct3",
       tasks: [
-        { name: 't31', description: 'd31' },
-        { name: 't32', description: 'd32' },
+        { name: "t31", description: "d31" },
+        { name: "t32", description: "d32" },
       ],
     },
   ];
---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare as todoSymbol } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    setUser(getLocalUser());
+  }, []);
+
+  return (
+    <header className="header">
+      <h1 className="heading">
+        <FontAwesomeIcon icon={todoSymbol} /> Todo List
+      </h1>
+      <StatusBar user={user} setUser={setUser} />
+    </header>
+  );
+}
+
+function StatusBar({ user, setUser }) {
+  return (
+    <div className="status-bar">
+      <div className="username">{user}</div>
+      <button className="button" onClick={editUser}>
+        Edit
+      </button>
+    </div>
+  );
+
+  function editUser() {
+    const newUser = prompt("enter the name of new user");
+    if (newUser) {
+      setUser(newUser);
+      localStorage.setItem("todo-user", newUser);
+    }
+  }
+}
+
+function getLocalUser() {
+  if (localStorage.hasOwnProperty("todo-user")) {
+    return localStorage.getItem("todo-user");
+  }
+  return "Guest";
+}
+export default Header;
